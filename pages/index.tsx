00db6fdf96e6187bf86d1d5d14f12d147b99fb5c
neomond <neoweise@yahoo.com>
2023-04-05 21:52:40 +0400

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+export default function Home() {
+  return (
+    <>
+      <main>Hello, world!</main>
+    </>
+  );
+}

47655dd6faa61808e157dfe441ee9b003f358f1b
neomond <neoweise@yahoo.com>
2023-04-05 23:04:43 +0400

    Adding News Article component entry

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,40 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { GetServerSideProps } from "next";
+import { NewsArticle, NewsResponse } from "@/models/NewsArticles";
+import NewsArticleEntry from "@/components/NewsArticleEntry";
 
-export default function Home() {
+interface BreakingNewsPageProps {
+  newsArticles: NewsArticle[];
+}
+
+export const getServerSideProps: GetServerSideProps<
+  BreakingNewsPageProps
+> = async () => {
+  const response = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=" +
+      process.env.NEWS_API_KEY
+  );
+  const newsResponse: NewsResponse = await response.json();
+
+  return {
+    props: { newsArticles: newsResponse.articles },
+  };
+};
+
+export default function BreakingNewsPage({
+  newsArticles,
+}: BreakingNewsPageProps) {
   return (
     <>
-      <main>Hello, world!</main>
+      <Head>
+        <title key="title"> Breaking News - NextJS News App</title>
+      </Head>
+      <main>
+        <h1>Breaking News</h1>
+        <NewsArticleEntry article={newsArticles[0]} />
+      </main>
     </>
   );
 }

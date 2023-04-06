f623d0cf65532ff2ffd73ed3ca74419243c68638
neomond <neoweise@yahoo.com>
2023-04-06 19:39:28 +0400

    adding search

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,40 @@
+import { NewsArticle } from "@/models/NewsArticles";
+import { FormEvent, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
 const SearchNewsPage = () => {
+  const [searchResults, setSearchResults] = useState<NewsArticle[] | null>(
+    null
+  );
+  const [searchResultsLoading, setSearchResultsLoading] = useState(false);
+  const [searchResultsLoadingIsError, setSearchResultsLoadingIsError] =
+    useState(false);
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const searchQuery = formData.get("searchQuery")?.toString().trim();
+
+    if (searchQuery) {
+      alert(searchQuery);
+    }
+  }
+
   return (
     <main>
       <h1>Search News</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="search-input">
+          <Form.Label>Search query</Form.Label>
+          <Form.Control
+            name="searchQuery"
+            placeholder="E.g. politics, sports, ..."
+          />
+        </Form.Group>
+        <Button type="submit" className="mb-3" disabled={searchResultsLoading}>
+          Search
+        </Button>
+      </Form>
     </main>
   );
 };
